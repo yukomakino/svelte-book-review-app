4959bbc559a68810a8695cc0d998b09bc7a1d74e
yuko makino <yucom2.99@gmail.com>
2022-05-17 11:48:06 +0900

    storeの作成

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,52 +1,51 @@
 <script lang="ts">
-  import SearchBar from '../components/SearchBar.svelte';
-import Spinner from '../components/Spinner.svelte';
-  import type { BookItem, Result } from '../repositories/book';
-  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory';
-  import BookCard from '../components/BookCard.svelte';
-  import InfiniteScroll from 'svelte-infinite-scroll';
+  import SearchBar from "../components/SearchBar.svelte";
+  import Spinner from "../components/Spinner.svelte";
+  import BookCard from "../components/BookCard.svelte";
+  import RepositoryFactory, { BOOK } from "../repositories/RepositoryFactory";
+  import InfiniteScroll from "svelte-infinite-scroll";
+  import { books } from "../store/book";
 
   const BookRepository = RepositoryFactory[BOOK];
 
-  let q = '';
+  let q = "";
   let empty = false;
-  let books: BookItem[] = [];
-  let promise: Promise<void>
-  let startIndex = 0;
+  let promise: Promise<void>;
   let totalItems = 0;
+  let startIndex = 0;
 
-  $: hasMore = totalItems > books.length;
+  $: hasMore = totalItems > $books.length;
 
   const handleSubmit = () => {
-    if (!q.trim()) return
-    promise = getBooks()
-  }
+    if (!q.trim()) return;
+    promise = getBooks();
+  };
 
   const getBooks = async () => {
-    books = []
-    empty = false
-    startIndex = 0
-    const result = await BookRepository.get({ q })
-    empty = result.totalItems === 0
-    totalItems = result.totalItems
-    books = result.items
-  }
+    books.reset();
+    empty = false;
+    startIndex = 0;
+    const result = await BookRepository.get({ q });
+    empty = result.totalItems === 0;
+    totalItems = result.totalItems;
+    books.add(result.items);
+  };
 
   const handleLoadMore = () => {
-    startIndex +=10
-    promise = getNextPage()
-  }
+    startIndex += 10;
+    promise = getNextPage();
+  };
 
   const getNextPage = async () => {
-    const result = await BookRepository.get({ q, startIndex })
+    const result = await BookRepository.get({ q, startIndex });
 
     // 取得データが既に存在するものを含む可能性があるので、idでフィルタリング。
-    const bookIds = books.map(book => book.id)
-    const filteredItems = result.items.filter(item => {
-      return !bookIds.includes(item.id)
-    })
-    books = [...books, ...filteredItems]
-  }
+    const bookIds = $books.map((book) => book.id);
+    const filteredItems = result.items.filter((item) => {
+      return !bookIds.includes(item.id);
+    });
+    books.add(filteredItems);
+  };
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
@@ -54,24 +53,23 @@ import Spinner from '../components/Spinner.svelte';
 </form>
 
 <div class="text-center mt-4">
-    {#if empty}
+  {#if empty}
     <div>検索結果が見つかりませんでした。</div>
   {:else}
-  <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
-    {#each books as book (book.id)}
-      <BookCard {book} />
-    {/each}
-  </div>
-  <InfiniteScroll window threshold={100} on:loadMore={handleLoadMore} />
+    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+      {#each $books as book (book.id)}
+        <BookCard {book} />
+      {/each}
+    </div>
+    <InfiniteScroll window threshold={100} on:loadMore={handleLoadMore} />
   {/if}
   {#await promise}
-    <div>{book.volumeInfo.title}</div>
     <div class="flex justify-center">
       <Spinner />
     </div>
-    {:catch e}
-      <span class="text-red-600 text-sm">
-        {e.message}
-      </span>
+  {:catch e}
+    <span class="text-red-600 text-sm">
+      {e.message}
+    </span>
   {/await}
 </div>
---
+++ src/pages/SearchBooks.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import BookCard from '../components/BookCard.svelte'
+  import { books } from '../store/book'
+  import { onDestroy } from 'svelte'
+  import type { BookItem } from '../repositories/book'
+
+  let _books: BookItem[]
+
+  const unsubscribe = books.subscribe(value => _books = value)
+
+  onDestroy(unsubscribe)
+</script>
+
+<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+  {#each _books as book (book.id)}
+    <BookCard {book} />
+  {/each}
+</div>
